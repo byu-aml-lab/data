88e3c3bac64bbbb15a0c67d248c0d55b7b09e78c
Jeffrey Lund <jefflund@gmail.com>
2013-07-25 15:59:38 -0700

    Added generate_document

--- test/generate.py
+++ test/generate.py
@@ -1,6 +1,11 @@
 import string
 import random
 
+ending_punctuation_data = '......!?' # make period much more common
+
+def roll_xdy(x, y):
+    return sum(random.randint(1, y) for _ in range(x))
+
 def load_language(filename):
     text = open(filename).read()
     text = text.strip()
@@ -8,13 +13,24 @@ def load_language(filename):
     text = text.translate(None, string.punctuation)
     return text.split()
 
-def generate_sentence(language, size):
-    language = list(language) # since we'll sample without replacement
-    sentence = []
+def generate_sentence(language):
+    # Copy language since we'll sample without replacement.
+    language = list(language)
 
-    for _ in range(size):
+    sampled_words = []
+
+    for _ in range(roll_xdy(2, 5)):
         word = random.choice(language)
         language.remove(word)
-        sentence.append(word)
+        sampled_words.append(word)
 
-    return ' '.join(sentence).capitalize() + random.choice('...!?')
+    end_punctionation = random.choice(ending_punctuation_data)
+    return ' '.join(sampled_words).capitalize() + end_punctionation
+
+def generate_document(language):
+    sentences = []
+
+    for _ in range(roll_xdy(3, 3)):
+        sentences.append(generate_sentence(language))
+
+    return ' '.join(sentences)

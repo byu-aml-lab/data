9f570637d5b421fba216cc3ec5896af3d50a4151
Jeffrey Lund <jefflund@gmail.com>
2013-07-26 19:42:58 -0700

    Refactored generate functions using random.sample

--- test/generate.py
+++ test/generate.py
@@ -1,8 +1,6 @@
 import string
 import random
 
-ending_punctuation_data = '......!?' # make period much more common
-
 def roll_xdy(x, y):
     return sum(random.randint(1, y) for _ in range(x))
 
@@ -14,23 +12,11 @@ def load_language(filename):
     return text.split()
 
 def generate_sentence(language):
-    # Copy language since we'll sample without replacement.
-    language = list(language)
-
-    sampled_words = []
-
-    for _ in range(roll_xdy(2, 5)):
-        word = random.choice(language)
-        language.remove(word)
-        sampled_words.append(word)
-
-    end_punctionation = random.choice(ending_punctuation_data)
-    return ' '.join(sampled_words).capitalize() + end_punctionation
+    words = random.sample(language, roll_xdy(2, 5))
+    ending_punctuation = random.choice('.....!?')
+    return ' '.join(words).capitalize() + ending_punctuation
 
 def generate_document(language):
-    sentences = []
-
-    for _ in range(roll_xdy(3, 3)):
-        sentences.append(generate_sentence(language))
+    return ' '.join(generate_sentence(language) for _ in range(roll_xdy(3, 3)))
 
     return ' '.join(sentences)
